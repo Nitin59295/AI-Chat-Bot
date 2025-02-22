900ab47f54aa241e956968ed3e0c6ec406a6c759
Nitin bhatia <134129893+Nitin59295@users.noreply.github.com>
2025-02-23 03:04:00 +0530

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import secrets
 
 class Config:
     SECRET_KEY = secrets.token_hex(16) # For Flask security
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:qwerty@localhost:5432/Aibot"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:passowrd@localhost:5432/your_db_name"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GEMINI_API_KEY = "AIzaSyBR_CHlj0EEs3i7khkosrvICZyEq-RLglU"  # Replace with your actual API key
-    JWT_SECRET_KEY = "2d19bf1cd8c2f7a4eb7eba1411a1f2b79039074df3df3fcd1eb3087d0658eb4c"
+    GEMINI_API_KEY = "your Gemini key"  # Replace with your actual API key
+    JWT_SECRET_KEY = "your Jwt token key"
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # ✅ Tokens expire in 1 hour
